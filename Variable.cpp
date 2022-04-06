9a3e56912ffda07a0aad8cbdf11c55cc59fa1ec1
DarrenSantiago <103085753+DarrenSantiago@users.noreply.github.com>
2022-04-05 20:29:02 -0700

    Create Variable.cpp

---
+++ Variable.cpp
@@ -0,0 +1,12 @@
+//header
+#include <iostream>
+using namespace std;
+int main() {//begin of function
+    float angka;
+    string name; //deklarasi variabel
+    nama = "darren santiago"; //masukan value ke dalam variabel
+    angka = 10,21+3,14
+    cout<<nama<<endl; //menampilkan variabel
+    cout<<angka;
+    
+}//end of function
