4f54cb5060265d795098c20e6afb32643614c1ca
DarrenSantiago <103085753+DarrenSantiago@users.noreply.github.com>
2022-04-26 19:15:42 -0700

    Create whilepart1.cpp

---
+++ whilepart1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+    int number = 0;
+    while (number ++ <10){
+        cout<<"3"<<"*"<<number<<"="<<number*3<<endl;
+    }
+    cout<<endl<<endl;
+    int number2 = 0;
+    while (number2 ++ <10){
+        cout<<"1"<<"*"<<number2<<"="<<number2*1<<endl;
+    }
+    int number3 = 0;
+    while (number3 ++ <10){
+        cout<<"2"<<"*"<<number3<<"="<<number3*2<<endl;
+    }
+    int number4 = 0;
+    while (number4 ++ <10){
+        cout<<"4"<<"*"<<number4<<"="<<number4*4<<endl;
+    }
+    int number5 = 0;
+    while (number5 ++ <10){
+        cout<<"5"<<"*"<<number5<<"="<<number5*5<<endl;
+    }
+    
+    return 0;
+}
+
+
+
